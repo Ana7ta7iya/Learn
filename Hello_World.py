f4c88e6f568af48bf171539f714558ee7ab48b03
Ana7ta7iya <48600605+Ana7ta7iya@users.noreply.github.com>
2020-12-02 21:41:20 -0800

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,7 @@
 print("Hello World")
+
+#hello world was the first printing option
+#no more lessons've been assigned
+print("hello wonderful experience ")
+print("none of the above + b")
+print('one two')
